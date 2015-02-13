2717730ff1ce08c33bb4031bafdc181cdea95eff
James <james@resin.io>
2015-02-13 16:11:46 +0000

    Set public access file node binary

--- build.sh
+++ build.sh
@@ -17,4 +17,4 @@ cd node \
 # Upload to S3
 
 sed -i -e "s/ACCESS/$ACCESS_KEY/" -e "s/SECRET/$SECRET_KEY/" /.s3cfg
-s3cmd put -c /.s3cfg node/$TAR_FILE s3://$BUCKET_NAME/node/v$NODE_VERSION/
+s3cmd -P put -c /.s3cfg node/$TAR_FILE s3://$BUCKET_NAME/node/v$NODE_VERSION/
