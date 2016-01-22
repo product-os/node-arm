b2e50fb961138c338a2ade460853582b253b769a
James <james@resin.io>
2016-01-23 06:16:43 +0700

    Set build flags for armel

--- armel/build.sh
+++ armel/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# comparing version: http://stackoverflow.com/questions/16989598/bash-comparing-version-numbers
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" == "$1"; }
+
 # set env var
 NODE_VERSION=$1
 ARCH=arm
@@ -8,10 +11,17 @@ ARCH_VERSION=armel
 TAR_FILE=node-v$NODE_VERSION-linux-$ARCH_VERSION
 BUCKET_NAME=$BUCKET_NAME
 
+# --with-arm-fpu flag is not available for node versions 0.12.x and 0.10.x
+if version_le "$NODE_VERSION" "4"; then
+	BUILD_FLAGs='--with-arm-float-abi=softfp --with-arm-fpu=vfp'
+else
+	BUILD_FLAGs='--with-arm-float-abi=softfp'
+fi
+
 # compile node
 cd node \
 	&& git checkout v$NODE_VERSION \
-	&& ./configure --without-snapshot --dest-cpu=$ARCH \
+	&& ./configure --without-snapshot --dest-cpu=$ARCH $BUILD_FLAGs \
 	&& make install -j$(nproc) DESTDIR=$TAR_FILE V=1 PORTABLE=1 \
 	&& cp LICENSE $TAR_FILE \
 	&& tar -cvzf $TAR_FILE.tar.gz $TAR_FILE \
