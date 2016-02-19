50a5d5f8c3d08f33f69b9b773141b6db2ff25221
James <james@resin.io>
2016-02-19 18:20:59 +0700

    Generate checksums along with the binaries

--- armv7hf/build.sh
+++ armv7hf/build.sh
@@ -13,8 +13,11 @@ cd node \
 	&& git checkout v$NODE_VERSION \
 	&& make -j$(nproc) binary DESTCPU=$ARCH \
 	&& mv node-v$NODE_VERSION-linux-$ARCH.tar.gz $TAR_FILE \
+	&& curl -SLO "http://resin-packages.s3.amazonaws.com/SHASUMS256.txt" \
+	&& sha256sum $TAR_FILE >> SHASUMS256.txt \
 	&& cd /
 
 # Upload to S3 (using AWS CLI)
 printf "$ACCESS_KEY\n$SECRET_KEY\n$REGION_NAME\n\n" | aws configure
 aws s3 cp node/$TAR_FILE s3://$BUCKET_NAME/node/v$NODE_VERSION/
+aws s3 cp node/SHASUMS256.txt s3://$BUCKET_NAME/
