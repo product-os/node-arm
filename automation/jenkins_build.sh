2c8e1dcc0b30422273bb087e2a5fc1fd8fb94b0d
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-10-26 10:12:40 +0700

    specify suite when compile debian armv7hf node

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" != "$1"; }
+
 cp -f commit-table $DISTRO/$ARCH
 cd $DISTRO
 cd $ARCH
@@ -8,6 +10,14 @@ docker build --no-cache=true -t node-$DISTRO-$ARCH-builder .
 
 for NODE_VERSION in $NODE_VERSIONS
 do
+	if [ $DISTRO == 'debian' ] && [ $ARCH == 'armv7hf' ]; then
+		if version_le $NODE_VERSION 4; then
+			sed -e s~#{SUITE}~wheezy~g Dockerfile.tpl > Dockerfile
+		else
+			sed -e s~#{SUITE}~jessie~g Dockerfile.tpl > Dockerfile
+		fi
+		docker build --no-cache=true -t node-$DISTRO-$ARCH-builder .		
+	fi
 	docker run --rm -e ACCESS_KEY=$ACCESS_KEY \
 					-e SECRET_KEY=$SECRET_KEY \
 					-e BUCKET_NAME=$BUCKET_NAME node-$DISTRO-$ARCH-builder bash build.sh $NODE_VERSION
